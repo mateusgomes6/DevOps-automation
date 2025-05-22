3412ff1c2ab44582e1095eea22a765a649857b7f
Mateus Gomes de carvalho <mateusgomesdc@hotmail.com>
2025-05-22 18:28:06 -0300

    feat: add train custom

--- log-analysis.py
+++ log-analysis.py
@@ -23,3 +23,32 @@ class LogAnalyzer:
                      loss='categorical_crossentropy',
                      metrics=['accuracy'])
         return model
+    
+    def process_logs(self):
+        for log in self.log_processor.stream_logs():
+            inputs = self.tokenizer.encode_plus(
+                log['message'],
+                max_length=512,
+                pad_to_max_length=True,
+                return_tensors='tf')
+                
+            prediction = self.model.predict([inputs['input_ids'],
+                                           inputs['attention_mask']])
+            label = np.argmax(prediction)
+            
+            if label == 4:  # Critical error
+                self.trigger_incident_response(log)
+            elif label == 3:  # Warning
+                self.send_alert(log)
+                
+    def train_custom_model(self, labeled_logs):
+        texts = [x['text'] for x in labeled_logs]
+        labels = [x['label'] for x in labeled_logs]
+        
+        inputs = self.tokenizer(texts, padding=True, truncation=True,
+                               max_length=512, return_tensors='tf')
+        
+        self.model.fit([inputs['input_ids'], inputs['attention_mask']],
+                      tf.one_hot(labels, depth=5),
+                      epochs=3,
+                      batch_size=8)
