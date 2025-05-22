ecd1a9f2997cc6b99ce80afbac0d4bbfc7bd95d2
Mateus Gomes de carvalho <mateusgomesdc@hotmail.com>
2025-05-22 18:16:54 -0300

    feat: add clustering

--- container-manager.py
+++ container-manager.py
@@ -18,3 +18,29 @@ class ContainerOrchestrator:
         value = Dense(1)(x)
         
         policy = Dense(5, activation='softmax')(x)
+    
+    def get_cluster_state(self):
+        nodes = self.kubernetes.list_node()
+        pods = self.kubernetes.list_pod_for_all_namespaces()
+        
+        state = []
+        for node in nodes.items:
+            node_metrics = self.get_node_metrics(node.metadata.name)
+            state.extend([
+                node_metrics['cpu'],
+                node_metrics['memory'],
+                node_metrics['pods'],
+                node_metrics['network'],
+                node_metrics['disk']
+            ])
+        return np.array(state)
+    
+    def take_action(self, action):
+        if action == 1:
+            self.scale_services()
+        elif action == 2:
+            self.optimize_resources()
+        elif action == 3:
+            self.rebalance_pods()
+        elif action == 4:
+            self.perform_maintenance()
