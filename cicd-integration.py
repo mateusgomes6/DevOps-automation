d66d12b05dc87fdf4514898e99db4cf2d4646177
Mateus Gomes de carvalho <mateusgomesdc@hotmail.com>
2025-05-22 19:05:31 -0300

    feat: add features

--- cicd-integration.py
+++ cicd-integration.py
@@ -1,3 +1,4 @@
+import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense, Dropout
 
@@ -20,3 +21,31 @@ class CICDOrchestrator:
                      loss='binary_crossentropy',
                      metrics=['accuracy'])
         return model
+    
+    def analyze_code_changes(self, commit):
+        complexity = calculate_cyclomatic_complexity(commit.diff)
+        test_coverage = get_test_coverage(commit)
+        deprecations = check_deprecations(commit)
+        
+        features = np.array([complexity, test_coverage, deprecations] + 
+                          [0]*17)
+        
+        risk_score = self.model.predict(features.reshape(1, -1))
+        return risk_score[0][0]
+    
+    def smart_deployment(self, pipeline):
+        risk_score = self.analyze_code_changes(pipeline.commit)
+        
+        if risk_score < 0.3:
+            self.execute_deployment(pipeline, 'production')
+        elif 0.3 <= risk_score < 0.7:
+            self.execute_canary(pipeline)
+        else:
+            self.execute_staging(pipeline)
+            self.enhance_monitoring(pipeline)
+    
+    def update_model(self, deployment_result):
+        self.performance_metrics.append(deployment_result)
+        if len(self.performance_metrics) >= 100:
+            X, y = self.prepare_training_data()
+            self.model.fit(X, y, epochs=5)
