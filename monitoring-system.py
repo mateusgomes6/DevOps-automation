752533fee77073cd447f8ebc4d2bc49bb52bcfb2
Mateus Gomes de carvalho <mateusgomesdc@hotmail.com>
2025-05-22 17:17:52 -0300

    feat: add build model and setup metrics

--- monitoring-system.py
+++ monitoring-system.py
@@ -16,3 +16,24 @@ class AdvancedMonitoring:
         self.model = self.build_advanced_model()
         self.metrics_server = server(8000)
         self.setup_custom_metrics()
+
+    def build_advanced_model(self):
+        inputs = keras.Input(shape=(60, 15))
+        x = LSTM(128, return_sequences=True)(inputs)
+        x = Attention()([x, x])
+        x = layers.Dense(64, activation='relu')(x)
+        outputs = layers.Dense(3, activation='softmax')(x)
+
+        model = keras.Model(inputs=inputs, outputs=outputs)
+        model.compile(optimizer='adam',
+                      loss='categorical_crossentropy',
+                      metrics=['accuracy', keras.metrics.AUC()])
+        return model
+    
+    def setup_custom_metrics(self):
+        self.cpu_prediction = prometheus_client.Gauge(
+            'cpu_anomaly_score', 'Prediction score for CPU anomalies'
+        )
+        self.mem_prediction = prometheus_client.Gauge(
+            'mem_anomaly_score', 'Prediction score for Memory anomalies'
+        )
